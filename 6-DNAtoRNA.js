412c73a4ad7c7073bf9af8e61d3620cc5b828fe5
riyandarmawan <riyandarmawan042@gmail.com>
2024-02-06 11:53:32 +0700

    Day 7 - Count by X

--- 6-DNAtoRNA.js
+++ 6-DNAtoRNA.js
@@ -3,12 +3,33 @@
 // https://www.codewars.com/kata/5556282156230d0e5e000089
 // DNA to RNA Conversion
 
-function DNAtoRNA(dna) {
-    let seperate = dna.split('');
-    
-    for(let i = 0; i < seperate.length; i++) {
-        if(seperate[i] == "T") {
-            seperate[i] = "U";
-        }
-    }
-}
+// function DNAtoRNA(dna) {
+//     let seperate = dna.split('');
+
+//     for(let i = 0; i < seperate.length; i++) {
+//         if(seperate[i] == "T") {
+//             seperate[i] = "U";
+//         }
+//     }
+// }
+
+// function DNAtoRNA(dna) {
+//   let rna = '';
+//   for (let i = 0; i < dna.length; i++) {
+//     rna += dna[i] === 'T' ? 'U' : dna[i];
+//   }
+//   return rna;
+// }
+
+// const DNAtoRNA = (dna) =>
+//   dna
+//     .split('')
+//     .map((el) => (el === 'T' ? 'U' : el))
+//     .join('');
+
+// const DNAtoRNA = (dna) => dna.replace(/T/g, 'U');
+// const DNAtoRNA = (dna) => dna.replaceAll('T', 'U');
+
+const DNAtoRNA = (dna) => dna.split('T').join('U');
+
+console.log(DNAtoRNA("GCAT"));
---
+++ 7-countBy.js
@@ -0,0 +1,21 @@
+// function countBy(x, n) {
+//     let z = [x];
+
+//     for(let i = 0; i < (n - 1); i++) {
+//         z.push(z[i] + x)
+//     }
+
+//     return z;
+// }
+
+const countBy = (x, n) => {
+    let z = [x];
+
+    for(let i = 0; i < (n - 1); i++) {
+        z.push(z[i] + x)
+    }
+
+    return z;
+}
+
+console.log(countBy(2, 5));
