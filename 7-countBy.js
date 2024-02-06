1b8e4ad3e65e6619bb59ef5d8d1f5b1be714163a
riyandarmawan <riyandarmawan042@gmail.com>
2024-02-06 11:54:00 +0700

    Day 7 - Count by X

--- 7-countBy.js
+++ 7-countBy.js
@@ -1,3 +1,8 @@
+// WPU Coding Challenge 2024
+// 7/366
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e
+// Count by X
+
 // function countBy(x, n) {
 //     let z = [x];
 
@@ -11,11 +16,11 @@
 const countBy = (x, n) => {
     let z = [x];
 
-    for(let i = 0; i < (n - 1); i++) {
-        z.push(z[i] + x)
+    for (let i = 0; i < n - 1; i++) {
+        z.push(z[i] + x);
     }
 
     return z;
-}
+};
 
 console.log(countBy(2, 5));
