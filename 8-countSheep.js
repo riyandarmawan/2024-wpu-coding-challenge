b1134d06166c8c368d0e306ec13821f6fb6c9978
Riyan042 <riyandarmawan042@gmail.com>
2024-02-07 22:42:07 +0700

    Day 9 - Find Maximum and Minimum Values of a List

--- 8-countSheep.js
+++ 8-countSheep.js
@@ -3,14 +3,25 @@
 // https://www.codewars.com/kata/5513795bd3fafb56c200049e
 // If you can't sleep, just count sheep!!
 
-var countSheep = function (num) {
-    let sheep = "";
+// var countSheep = function (num) {
+//     let sheep = "";
 
-    for (let i = 1; i <= num; i++) {
-        sheep += i + " sheep...";
-    }
+//     for (let i = 1; i <= num; i++) {
+//         sheep += i + " sheep...";
+//     }
 
-    return sheep;
-};
+//     return sheep;
+// };
+
+// wpu
+// var countSheep = function (num) {
+//   let result = '';
+//   for (let i = 1; i <= num; i++) {
+//     result += `${i} sheep...`;
+//   }
+//   return result;
+// };
+
+const countSheep = (num) => [...Array(num)].map((_, i) => `${i + 1} sheep...`).join('');
 
 console.log(countSheep(3));
