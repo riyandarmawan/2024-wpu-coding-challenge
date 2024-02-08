c0a9dd2bf6a4430e25bf2dafbf0941843338e4d7
Riyan042 <riyandarmawan042@gmail.com>
2024-02-08 08:54:08 +0700

    Day 10 - Convert a string to an array

---
+++ 10-stringToArray.js
@@ -0,0 +1,14 @@
+// WPU Coding Challenge 2024
+// 10/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+// Convert a string to an array
+
+// function stringToArray(string) {
+//     let list = string.split(' ');
+    
+//     return list;
+// }
+
+const stringToArray = (string) => string.split(' ');
+
+console.log(stringToArray("I love arrays they are my favorite"));
