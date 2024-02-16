6fe234ba94e2519a297868432c58e9b8c3974485
riyandarmawan <riyandarmawan042@gmail.com>
2024-02-17 06:54:19 +0700

    Day 18 - Grasshopper - Personalized Message

---
+++ 18-greet.js
@@ -0,0 +1,27 @@
+// WPU Coding Challenge 2024
+// 18/366
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7
+// Grasshopper - Personalized Message
+
+// function greet (name, owner) {
+//   return name === owner ? 'Hello boss' : 'Hello guest';
+// }
+
+// const greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest';
+
+// wpu
+// function greet(name, owner) {
+//   if (name === owner) {
+//     return 'Hello boss';
+//   } else {
+//     return 'Hello guest';
+//   }
+// }
+
+// const greet = (name, owner) => (name === owner ? 'Hello boss' : 'Hello guest');
+
+// const greet = (name, owner) => `Hello ${name === owner ? 'boss' : 'guest'}`;
+
+const greet = (name, owner) => 'Hello ' + ['guest', 'boss'][+(name === owner)];
+
+console.log(greet('Daniel', 'Daniel'));
