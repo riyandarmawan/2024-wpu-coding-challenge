c554af7ace736df8e2fc66799a1733cd257c80a7
Riyan042 <riyandarmawan042@gmail.com>
2024-02-27 19:39:56 +0700

    Day 22 - Remove exclamation marks

---
+++ 22-removeExclamationMarks.js
@@ -0,0 +1,12 @@
+// WPU Coding Challenge 2024
+// 22/366
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+// Remove exclamation marks
+
+// function removeExclamationMarks(s) {
+//     return s.replaceAll('!', '');
+// }
+
+const removeExclamationMarks = (s) => s.replaceAll('!', '');
+
+console.log(removeExclamationMarks("Hello World!"));
