e436169e7b89d11eafbf4ec003e32664455a95ab
Riyan042 <riyandarmawan042@gmail.com>
2024-02-27 20:02:03 +0700

    Day 24/366 - Quarter of the year

---
+++ 24-quarterOf.js
@@ -0,0 +1,42 @@
+// WPU Coding Challenge 2024
+// 24/366
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af
+// Quarter of the year
+
+// const quarterOf = (month) => {
+//     if (month <= 3) return 1;
+//     if (month <= 6) return 2;
+//     if (month <= 9) return 3;
+//     if (month <= 12) return 4;
+// };
+
+// const quarterOf = (month) => {
+//     if (month <= 3) return 1;
+//     if (month <= 6) return 2;
+//     if (month <= 9) return 3;
+
+//     return 4;
+// };
+
+// const quarterOf = (month) => (month <= 3) ? 1 : (month <= 6) ? 2 : (month <= 9) ? 3 : 4;
+
+// const quarterOf = (month) => Math.ceil(month / 3);
+
+// wpu
+// const quarterOf = (month) => {
+//   if (month <= 3) {
+//     return 1;
+//   } else if (month <= 6) {
+//     return 2;
+//   } else if (month <= 9) {
+//     return 3;
+//   } else {
+//     return 4;
+//   }
+// };
+
+// const quarterOf = (month) => (month <= 3 ? 1 : month <= 6 ? 2 : month <= 9 ? 3 : 4);
+
+const quarterOf = (month) => Math.ceil(month / 3);
+
+console.log(quarterOf(11));
