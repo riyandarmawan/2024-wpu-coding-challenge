27139d242b9415cf1687c3acffa9424658ba3f1f
Riyan042 <riyandarmawan042@gmail.com>
2024-03-15 09:38:45 +0700

    Day 31 - Third Angle of a Triangle

--- 30-enough.js
+++ 30-enough.js
@@ -11,6 +11,22 @@
 //     return wait - (cap - on);
 // }
 
-const enough = (cap, on, wait) => cap >= on + wait ? 0 : wait - (cap - on);
+// const enough = (cap, on, wait) => cap >= on + wait ? 0 : wait - (cap - on);
+
+// wpu
+// function enough(cap, on, wait) {
+//   if (cap >= on + wait) {
+//     return 0;
+//   } else {
+//     return wait - (cap - on);
+//   }
+// }
+
+function enough(cap, on, wait) {
+  return cap >= on + wait ? 0 : wait - (cap - on);
+  // return Math.max(wait + on - cap, 0);
+}
+
+// const enough = (cap, on, wait) => (cap >= on + wait ? 0 : wait - (cap - on));
 
 console.log(enough(20, 5, 5));
---
+++ 31-otherAngle.js
@@ -0,0 +1,19 @@
+// WPU Coding Challenge 2024
+// 31/366
+// https://www.codewars.com/kata/5a023c426975981341000014
+// Third Angle of a Triangle
+
+// function otherAngle(a, b) {
+//     return 180 - (a + b);
+// }
+
+// const otherAngle = (a, b) => 180 - (a + b);
+
+// wpu
+// function otherAngle(a, b) {
+//   return 180 - (a + b);
+// }
+
+const otherAngle = (a, b) => 180 - (a + b);
+
+console.log(otherAngle(30, 60));
