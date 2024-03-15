f333c88c41f8ae72ad8c2e2c34e2414a718bb741
Riyan042 <riyandarmawan042@gmail.com>
2024-03-15 11:33:30 +0700

    Day 34 - Twice as old

---
+++ 34-twiceAsOld.js
@@ -0,0 +1,20 @@
+// WPU Coding Challenge 2024
+// 34/366
+// https://www.codewars.com/kata/5b853229cfde412a470000d0
+// Twice as old
+
+// function twiceAsOld(dadYearsOld, sonYearsOld) {
+//     return Math.abs(dadYearsOld - sonYearsOld * 2);
+// }
+
+// const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - sonYearsOld * 2);
+
+// wpu
+// function twiceAsOld(dadYearsOld, sonYearsOld) {
+//   // your code here
+//   return Math.abs(dadYearsOld - sonYearsOld * 2);
+// }
+
+const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - sonYearsOld * 2);
+
+console.log(twiceAsOld(55, 30));
