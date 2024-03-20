321a8e858457af8e00bbce760d0fde488fcfae95
Riyan042 <riyandarmawan042@gmail.com>
2024-03-20 17:42:24 +0700

    39 - Volume of a Cuboid

---
+++ 39-getVolumeOfCuboid.js
@@ -0,0 +1,21 @@
+// WPU Coding Challenge 2024
+// 39/366
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+// Volume of a Cuboid
+
+// class Kata {
+//     static getVolumeOfCuboid(length, width, height) {
+//         return length * width * height;
+//     }
+// }
+
+// wpu
+// class Kata {
+//   static getVolumeOfCuboid(length, width, height) {
+//     return length * width * height;
+//   }
+// }
+
+const getVolumeOfCuboid = (l, w, h) => l * w * h;
+
+console.log(getVolumeOfCuboid(1, 2, 2));
