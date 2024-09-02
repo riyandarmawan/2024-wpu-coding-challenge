78a19a0e1599ff81fab7b85537f588a1085e98f4
riyandarmawan <riyandarmawan@gmail.com>
2024-09-02 20:36:26 +0700

    Day 43 - Is it even?

---
+++ 43-testEvent.js
@@ -0,0 +1,8 @@
+// WPU Coding Challenge 2024
+// 43/366
+// https://www.codewars.com/kata/555a67db74814aa4ee0001b5
+// is it even?
+
+const testEvent = (n) => n % 2 === 0;
+
+console.log(testEvent(0.5));
